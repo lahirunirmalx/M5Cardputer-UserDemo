ddbf58732b6e8f03c6da8aa33c705decb6bfc873
lahiru <lahirunirmalx@gmail.com>
2024-11-07 20:37:27 +0530

    add wifi auto config show status bar icon for background runing radio

--- main/hal/hal.h
+++ main/hal/hal.h
@@ -18,7 +18,13 @@
 #include <iostream>
 #include <string>
 
+
+// USER - CONFIG EDIT
 #define TONE_CHANNEL 6
+#define TIME_ZONE "UTC-5:30"
+#define WIFI_SSID "M5Stak"
+#define WIFI_PASS  "abc1234"
+// USER - CONFIG EDIT
 
 namespace HAL
 {
@@ -41,12 +47,18 @@ namespace HAL
         SDCard* _sdcard;
 
         bool _sntp_adjusted;
+        bool _web_redio_runing;
+        bool _wifi_connected;
+
+        char _wifi_ssid[50];
+        char _wifi_password[50];
 
     public:
         Hal()
             : _display(nullptr), _canvas(nullptr), _canvas_system_bar(nullptr), _canvas_keyboard_bar(nullptr),
               _keyboard(nullptr), _mic(nullptr), _speaker(nullptr), _homeButton(nullptr), _sdcard(nullptr),
-              _sntp_adjusted(false)
+              _sntp_adjusted(false),_web_redio_runing(false),_wifi_connected(false),_wifi_ssid(WIFI_SSID),
+              _wifi_password(WIFI_PASS)
         {
         }
 
@@ -63,6 +75,24 @@ namespace HAL
 
         inline void setSntpAdjusted(bool isAdjusted) { _sntp_adjusted = isAdjusted; }
         inline bool isSntpAdjusted(void) { return _sntp_adjusted; }
+
+        inline void setWebRedioRuning(bool isWebRedioRuning) { _web_redio_runing = isWebRedioRuning; }
+        inline bool isWebRedioRuning(void) { return _web_redio_runing; }
+
+        inline void setWifiConnected(bool isWifiConnected) { _wifi_connected = isWifiConnected; }
+        inline bool isWifiConnected(void) { return _wifi_connected; }
+
+        inline void setWifiSSID(const char* wifi_ssid) {
+          strncpy(_wifi_ssid, wifi_ssid, sizeof(_wifi_ssid) - 1);
+         _wifi_ssid[sizeof(_wifi_ssid) - 1] = '\0';  
+        }
+        inline const char* getWifiSSID() const {return _wifi_ssid;}
+
+        inline void setWifiPassword(const char* wifi_password) {
+          strncpy(_wifi_password, wifi_password, sizeof(_wifi_password) - 1);
+         _wifi_password[sizeof(_wifi_password) - 1] = '\0';  
+        }
+        inline const char* getWifiPassword() const {return _wifi_password;}
 
         // Canvas
         inline void canvas_system_bar_update() { _canvas_system_bar->pushSprite(_canvas_keyboard_bar->width(), 0); }
